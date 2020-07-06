951a0f165a018c090e00f0700f49072003036f2a
Agurshageok <39487380+Agurshageok@users.noreply.github.com>
2020-07-06 12:32:49 -0300

    Update comments, tidy up

--- TP Haskell.hs
+++ TP Haskell.hs
@@ -3,12 +3,15 @@ import Data.Char
 import Data.List
 import Data.Function
 
+{------------------------------
+funcion principal de ejecucion.
+------------------------------}
+
 main :: IO ()
 main = do { 
            str <- leerArchivoEntrada;
            esp <- ingresarCantEspacios;
-           -- putStrLn ((show esp) ++ "   espacios");
-           arbol <- transformar str; --IO [[String]]
+           arbol <- transformar str;
            validarOrden arbol;
            pathSalida <- obtenerRutaSalida;
            generarArchivoSalida pathSalida esp arbol;
@@ -18,21 +21,23 @@ main = do {
            else putStrLn "Fin del procesamiento."
            }
 
-{-----------------------------------------
-Validacion del Archivo de entrada
+{------------------------------------------------------------------------------------
+------------------------- Validacion del Archivo de entrada -------------------------
 Comprueba que el archivo de entrada contenga
 solamente caracteres AlphaNum o Dash
 Elimina las comas que separan los marcadores
 de nodos en cada linea.
 Validaciones: 
         -> Caracteres Solo "1...9" || "a...z" || "A...Z"
-        -> ultimo elemento de cada fila tiene el formato "X-N" donde N es un entero, X es un String y estan separados por 1 dash
+        -> ultimo elemento de cada fila tiene el formato "X-N" 
+                donde N es un entero, X es un String y estan separados por 1 dash
         -> No existen 2 filas iguales. 
-        -> 
+        -> Luego de la Transformacion del arbol, se valida el orden lexicografico, 
+        aprovechando que la estructura del arbol es mas apropiada para esa tarea.
 Deja una lista, donde cada elemento es una 
 lista de String, donde cada string es el 
 valor de cada nodo.
-------------------------------------------}
+------------------------------------------------------------------------------------}
 
 validarArchivo :: String -> IO ([[String]]) 
 validarArchivo str = do
@@ -88,12 +93,10 @@ isSorted [x]      = True
 isSorted (x:y:xs) = x <= y && isSorted (y:xs)
 
 filasConcat :: [[String]] -> [String]
---filasConcat archivo = map (\str -> takeWhile (/= '-') (concat str)) archivo
 filasConcat archivo = map concat archivo
 
 checkFilasNoRepetidos :: [[String]] -> Bool
 checkFilasNoRepetidos lines = length (filasConcat lines) == length (nub (filasConcat lines))
-        --(isSorted $ map head filas) && (checkFilasOrden (map tail filas))
 
 validarOrden ::  ArbN String -> IO ()
 validarOrden arb = do
@@ -107,13 +110,11 @@ okArbol arb = isSorted (map etiqueta (hijos arb)) && (all (==True) (okArbol2 (hi
 okArbol2 :: [ArbN String] -> [Bool]
 okArbol2 (h:hs) = (okArbol h):(okArbol2 hs)
 
-{-
------------------------------------------------------------------------------------
-Manejo de IO.
+{-----------------------------------------------------------------
+---------------------- Manejo de IO. -----------------------------
 Validaciones pertinentes para evitar errores de input humano.
 Manejo de "repeat", mas de un procesamiento por ejecucion. 
------------------------------------------------------------------------------------
--}
+-----------------------------------------------------------------}
 
 obtenerRutaSalida :: IO String
 obtenerRutaSalida = do{
@@ -128,7 +129,6 @@ manejarRutaInvalida e = do{
                            putStrLn("La ruta ingresada no es válida, excepción: " ++ (show e));
                            obtenerRutaSalida
                           }
-
 
 leerArchivoEntrada :: IO [[String]]
 leerArchivoEntrada = do{
@@ -158,7 +158,6 @@ manejarErrorArchivo2 e = do{
                            leerArchivoEntrada
                           }
 
-
 ingresarCantEspacios :: IO Int
 ingresarCantEspacios = do{
                           putStrLn "Ingresar la cantidad de espacios de indentación para el archivo de salida.";
@@ -174,7 +173,6 @@ ingresarCantEspacios = do{
 validarCantEspacios :: String -> Bool
 validarCantEspacios esp = length (words esp) == 1 && (all isDigit esp) && (read esp ::Int) > 0
 
-
 consultarFinDatos :: IO Bool
 --Devuelve True si se desea continuar ingresando archivos.
 consultarFinDatos = do{
@@ -188,13 +186,16 @@ consultarFinDatos = do{
                                }
                       }
 
-
 esSNValido :: String -> Bool
 esSNValido str = str == "S" || str == "N" || str == "s" || str == "n"
 
-{-----------------------------
+{---------------------------------
 -- Arbol y transformacion ----
-------------------------------}
+Seccion que se encarga de tomar 
+la informacion del archivo de 
+entrada y devolver un arbol bien 
+formado.
+---------------------------------}
 {-
 [["1","2","1","A-7"],["1","2","1","B-2"],["1","2","1","C-3"],["1","2","1","2","A-1"],["1","2","1","2","Z-5"],["1","2","1","3-7"],["1","2","2","1-0"],["1","2","2","2-3"],["1","2","5","1-1"],["2","1","1","1","W-5"],["2","1","1","1","Z-3"],["2","1","2","1-4"],["A","1","2","1","C-2"],["A","1","2","1","D-1"],["A","1","2","2","S-1"],["A","1","2","2","T-1"],["A","4","1","1","G-3"],["A","4","1","1","H-3"],["A","4","1","2","M-1"],["A","4","1","2","N-9"],["A","4","3","1-2"]]
 -}
@@ -205,7 +206,6 @@ etiqueta (Nodo e hs x) = e
 
 hijos :: ArbN a -> [ArbN a]
 hijos (Nodo e hs x) = hs
-
 
 existeHijo :: Eq a => a -> ArbN a -> Bool
 existeHijo hijo (Nodo e [] x) = False
@@ -226,8 +226,6 @@ getHijo :: Eq a => a -> ArbN a -> ArbN a
 --Devuelve el hijo del árbol que contiene la etiqueta del 1er parámetro. Antes debe usarse existeHijo.
 getHijo n (Nodo e (h:hs) x) = if n == (etiqueta h) then h else getHijo n (Nodo e hs x)
 
-
-
 transformar :: [[String]] -> IO (ArbN String)
 transformar xss = return (agregarFilas xss (Nodo "" [] 0))
 
@@ -240,21 +238,22 @@ cargarFila (n:ns) arb = if elem '-' n then agregarHijo arb (Nodo (takeWhile (/= 
                                       else if existeHijo n arb then reemplazarHijo arb (cargarFila ns (getHijo n arb))
                                                                else agregarHijo arb (cargarFila ns (Nodo n [] 0))
 
-
-
+{-----------------------------------------------------
+----------- Generar Archivo de Salida ----------------
+Seccion que se encarga de manejar posibles errores 
+a la hora de generar el archivo de salida, ademas
+de manejar donde y con que nombre se crea el archivo.
+-----------------------------------------------------}
 
 obtenerTotalNodo :: ArbN a -> Int
 obtenerTotalNodo (Nodo e hs x) = if null hs then x else sum(map obtenerTotalNodo hs)
-
 
 generarArchivoSalida :: String -> Int -> ArbN String -> IO ()
 generarArchivoSalida ruta esp arb =do
         writeFile ruta (arbToString esp 0 arb (esp + longMaxima arb esp) (longMayorTotal arb));
         putStrLn "Archivo Procesado con exito!"
 
-
 arbToString :: Int -> Int -> ArbN String -> Int -> Int -> String
-
 arbToString esp baseEsp (Nodo e hs x) longMax mayorTotal = if null hs
 
                                                            then e ++ 
@@ -274,24 +273,19 @@ arbToString esp baseEsp (Nodo e hs x) longMax mayorTotal = if null hs
 
 
 arbToStringL :: Int -> Int -> [ArbN String] -> Int -> Int -> String
-
 arbToStringL esp baseEsp [] longMax mayorTotal = ""
 arbToStringL esp baseEsp (h:hs) longMax mayorTotal = "\n" ++ concat(replicate (baseEsp + esp) " ") ++ (arbToString esp (esp + baseEsp) h longMax mayorTotal) ++ (arbToStringL esp baseEsp hs longMax mayorTotal)
 
 
 longMaxima :: ArbN String -> Int -> Int
 --Devuelve la máxima longitud en caracteres de un renglón del árbol.
-
 longMaxima (Nodo e [] x) esp = 7 + length e    -- Se suma 7 debido a la long de "Total: "
 longMaxima (Nodo e (h:hs) x) esp = length e + esp + maximum (map (\arb -> longMaxima arb esp) (h:hs))
 
 
 longMayorTotal :: ArbN a -> Int
 --Devuelve la longitud del valor de nodo más largo (en cantidad de caracteres)
-
 longMayorTotal (Nodo e [] x) = length (show x)
 longMayorTotal (Nodo e (h:hs) x) = max (length(show(obtenerTotalNodo (Nodo e (h:hs) x)))) (maximum (map (\arb -> length (show (obtenerTotalNodo arb))) (h:hs)))
-
-
 
 --(Nodo "" [Nodo "1" [Nodo "2" [Nodo "1" [Nodo "A" [] 7, Nodo "B" [] 2, Nodo "C" [] 3, Nodo "2" [Nodo "A" [] 1, Nodo "Z" [] 5] 0, Nodo "3" [] 7] 0, Nodo "2" [Nodo "1" [] 0, Nodo "2" [] 3] 0, Nodo "5" [Nodo "1" [] 1] 0] 0] 0, Nodo "2" [Nodo "1" [Nodo "1" [Nodo "1" [Nodo "W" [] 5, Nodo "Z" [] 3] 0] 0, Nodo "2" [Nodo "1" [] 4] 0] 0] 0, Nodo "A" [Nodo "1" [Nodo "2" [Nodo "1" [Nodo "C" [] 2, Nodo "D" [] 1] 0, Nodo "2" [Nodo "S" [] 1, Nodo "T" [] 1] 0] 0] 0, Nodo "4" [Nodo "1" [Nodo "1" [Nodo "G" [] 3, Nodo "H" [] 3] 0, Nodo "2" [Nodo "M" [] 1, Nodo "N" [] 9] 0] 0, Nodo "3" [Nodo "1" [] 2] 0] 0] 0] 0)
